02c1a64049f5f15ab49d5ea7ec1dbb34161b60c2
ctoKojo <cto.kojobot@gmail.com>
2025-05-02 23:33:57 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
     <div className="flex items-center justify-center my-6 w-full">
       <div className="p-4 rounded-xl">
         <img
-          src="/images/kojobot.png"
+          src="/kojobot.png"
           alt="Kojobot Logo"
           className="h-16 w-auto"
         />
