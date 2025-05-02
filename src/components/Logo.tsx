ac2b39077f437877bebc35b456ed1e7e6d84aca7
ctoKojo <cto.kojobot@gmail.com>
2025-05-02 23:29:49 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,14 @@
-
 import React from "react";
 
 const Logo: React.FC = () => {
   return (
-    <div className="flex items-center justify-center mb-6 w-full">
-      <div className="bg-white p-4 rounded-xl w-full">
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#61C9E0] to-[#6455F0] text-2xl font-bold">
-          KOJOBOT
-        </div>
+    <div className="flex items-center justify-center my-6 w-full">
+      <div className="p-4 rounded-xl">
+        <img
+          src="/images/kojobot.png"
+          alt="Kojobot Logo"
+          className="h-16 w-auto"
+        />
       </div>
     </div>
   );
